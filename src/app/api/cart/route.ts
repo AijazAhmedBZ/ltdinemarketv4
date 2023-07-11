ce1eaeda15e1de65e0ed2b7adcd7d6509a449972
aijazahmedbz <aijaz.ahmed.bz@gmail.com>
2023-07-11 10:29:24 +0500

    updated on 11-Jul-23

--- src/app/api/cart/route.ts
+++ src/app/api/cart/route.ts
@@ -7,14 +7,13 @@ import { eq } from "drizzle-orm";
 export const POST = async (request: NextRequest) => {
   const req = await request.json();
   const uid = uuid();
-  const setCookies = cookies();
-
   const user_id = cookies().get("user_id")?.value as string;
 
-  if (!user_id) {
+if (!user_id) {
+    const setCookies = cookies();
     setCookies.set("user_id", uid);
   }
-
+  
   try {
     const res = await db
       .insert(cartTable)
@@ -27,14 +26,20 @@ export const POST = async (request: NextRequest) => {
 
     return NextResponse.json({ res });
   } catch (error) {}
+  return NextResponse.json({ message: "Somthing Went Wrong" });
 };
 
-export const GET = async (request: NextRequest) => {
 
-  cookies().get("user_id")?.value as string;
+export const GET = async (request: NextRequest) => {
+  
+cookies().get("user_id")?.value as string;
+
+const getCookies = cookies()
+  
+console.log(getCookies)
 
   fetch(
-    `http://localhost:3000/api/cart?user_id=${cookies().get("user_id")?.value}`
+    `http://localhost:3000/api/cart?user_id=${cookies().get("user_id")?.value}`,{credentials:"include"}
   );
   const req = request.nextUrl;
   const uid = req.searchParams.get("user_id") as string;
